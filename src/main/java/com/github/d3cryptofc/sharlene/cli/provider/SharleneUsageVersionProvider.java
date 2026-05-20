032a434bd4e1ca566c860ba446699075b7df6d9e
Lelzin λ <d3cryptofc@gmail.com>
2026-05-20 18:39:44 -0300

    feat(provider): add version provider for program usage

---
+++ src/main/java/com/github/d3cryptofc/sharlene/cli/provider/SharleneUsageVersionProvider.java
@@ -0,0 +1,32 @@
+package com.github.d3cryptofc.sharlene.cli.provider;
+
+import picocli.CommandLine.IVersionProvider;
+import picocli.CommandLine.Model.CommandSpec;
+import picocli.CommandLine.Spec;
+
+/**
+ * Provides the current program usage version.
+ */
+public class SharleneUsageVersionProvider implements IVersionProvider {
+
+   @Spec
+   CommandSpec spec;
+
+   /**
+    * Gets the current program usage version (for example: {@code Program v1.x.y}).
+    *
+    * @return An array where each element is a line of text.
+    */
+   public String[] getVersion() {
+      // Get the command name.
+      String commandName = spec.qualifiedName();
+
+      // Get the current program version.
+      String programVersion = getClass()
+         .getPackage()
+         .getImplementationVersion();
+
+      // Returns an array containing the program usage version.
+      return new String[] {commandName + " v" + programVersion};
+   }
+}
