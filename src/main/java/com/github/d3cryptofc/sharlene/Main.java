c48465b7121e1e8218d96a4b3ab00af9985bff12
Lelzin λ <d3cryptofc@gmail.com>
2026-05-20 18:33:36 -0300

    feat(main): add main class with logger and OS name constants

---
+++ src/main/java/com/github/d3cryptofc/sharlene/Main.java
@@ -0,0 +1,22 @@
+package com.github.d3cryptofc.sharlene;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * The main program class.
+ */
+public class Main {
+
+   /**
+    * Global object logger.
+    */
+   public static final Logger LOGGER = LogManager.getLogger(Main.class);
+
+   /**
+    * Operating system name.
+    */
+   public static final String OSNAME = System.getProperty("os.name");
+
+   public static void main(String[] args) {}
+}
