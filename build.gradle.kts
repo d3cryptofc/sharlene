25954b3b3c3b8fa4d61ea73e38977ab140bd0d51
Lelzin λ <d3cryptofc@gmail.com>
2026-05-20 16:46:43 -0300

    deps: add `picocli` and codegen processor

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,12 +13,18 @@ repositories {
 dependencies {
     implementation("org.apache.logging.log4j:log4j-api:2.12.4")
     implementation("org.apache.logging.log4j:log4j-core:2.12.4")
+    implementation("info.picocli:picocli:4.7.7")
+    annotationProcessor("info.picocli:picocli-codegen:4.7.7")
 }
 
 java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(21)
     }
+}
+
+tasks.compileJava {
+    options.compilerArgs.add("-Aproject=${project.group}/${project.name}")
 }
 
 tasks.jar {
