3745c606518be7fb02c7e8f241a49490a1ae9db4
Lelzin λ <d3cryptofc@gmail.com>
2026-05-20 18:18:45 -0300

    feat(exception): add root unchecked exception class

---
+++ src/main/java/com/github/d3cryptofc/sharlene/exception/SharleneRuntimeException.java
@@ -0,0 +1,17 @@
+package com.github.d3cryptofc.sharlene.exception;
+
+/**
+ * The root unchecked exception of Sharlene.
+ */
+public class SharleneRuntimeException extends RuntimeException {
+
+   /**
+    * Constructs a new exception with a formatted message.
+    *
+    * @param format  A {@link String#format(String, Object...)} compatible format string.
+    * @param args    Arguments referenced by the format specifiers in the format string.
+    */
+   public SharleneRuntimeException(String format, Object... args) {
+      super(String.format(format, args));
+   }
+}
