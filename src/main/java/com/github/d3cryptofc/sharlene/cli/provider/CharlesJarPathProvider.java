44d89629939173ed237f7548d9a90ca5b8303e33
Lelzin λ <d3cryptofc@gmail.com>
2026-05-20 18:40:20 -0300

    feat(provider): add default charles jar path provider
    The new provider class introduces observable functionality by enabling
    the application to determine the default charles JAR path on different
    operating systems.

---
+++ src/main/java/com/github/d3cryptofc/sharlene/cli/provider/CharlesJarPathProvider.java
@@ -0,0 +1,29 @@
+package com.github.d3cryptofc.sharlene.cli.provider;
+
+import com.github.d3cryptofc.sharlene.Main;
+import com.github.d3cryptofc.sharlene.exception.UnsupportedOSException;
+
+/**
+ * Provides the default charles JAR file path for the current operating system.
+ */
+public class CharlesJarPathProvider {
+
+   /**
+    * Gets the default charles JAR file path for the current operating system.
+    *
+    * @throws UnsupportedOSException   If the current operating system is not supported
+    *                                  or cannot be detected.
+    */
+   public static String getDefault() throws UnsupportedOSException {
+      // Get the operating system name in lowercase.
+      String platform = Main.OSNAME.toLowerCase();
+
+      // Check OS is Linux.
+      if (platform.startsWith("linux")) {
+         return "/usr/share/charles-proxy/lib/charles.jar";
+      }
+
+      // If no return, throw unsupported OS.
+      throw new UnsupportedOSException();
+   }
+}
