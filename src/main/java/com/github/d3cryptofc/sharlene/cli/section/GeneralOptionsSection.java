48b2a1cf6073b8dbdc7ca9ab1e03a221b67fd0cc
Lelzin λ <d3cryptofc@gmail.com>
2026-05-20 18:23:14 -0300

    feat(cli): add general options section with help and version flags

---
+++ src/main/java/com/github/d3cryptofc/sharlene/cli/section/GeneralOptionsSection.java
@@ -0,0 +1,29 @@
+package com.github.d3cryptofc.sharlene.cli.section;
+
+import picocli.CommandLine.Option;
+
+/**
+ * This class represents the general options section of the CLI.
+ */
+public class GeneralOptionsSection {
+
+   /**
+    * Stores {@code true} when the help flag is used.
+    */
+   @Option(
+      names = {"-h", "--help"},
+      description = "Display this help information and exit",
+      usageHelp = true
+   )
+   public boolean helpRequested = false;
+
+   /**
+    * Stores {@code true} when the version flag is used.
+    */
+   @Option(
+      names = {"-V", "--version"},
+      description = "Display program version and exit",
+      versionHelp = true
+   )
+   public boolean versionRequested = false;
+}
