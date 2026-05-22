c5ea5a1fe1b0455966658e6763b320f49a92395a
Lelzin λ <d3cryptofc@gmail.com>
2026-05-22 09:39:50 -0300

    feat(logging): add utility class for logging
    - introduces `LogUtils` class with methods to log section separators
    - provides `section(Logger)` for standalone separator logging
    - provides `section(Logger, Runnable)` to wrap runnable execution with
      separators

---
+++ src/main/java/com/github/d3cryptofc/sharlene/utils/LogUtils.java
@@ -0,0 +1,39 @@
+package com.github.d3cryptofc.sharlene.utils;
+
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Utility class for logging.
+ */
+public class LogUtils {
+
+   /**
+    * The width of the section separator ({@value SEPARATOR_WIDTH}).
+    */
+   private static final int SEPARATOR_WIDTH = 60;
+
+   /**
+    * Logs a section separator using the given logger.
+    *
+    * @param logger The logger to use for logging.
+    */
+   public static void section(Logger logger) {
+      // Log the section separator.
+      logger.info("-".repeat(SEPARATOR_WIDTH));
+   }
+
+   /**
+    * Logs a section separator using the given logger, then runs the given runnable.
+    *
+    * @param logger   The logger to use for logging.
+    * @param runnable The runnable to run after logging the section separator.
+    */
+   public static void section(Logger logger, Runnable runnable) {
+      // Log the section separator before running the runnable.
+      section(logger);
+      // Run the runnable.
+      runnable.run();
+      // Log the section separator after running the runnable.
+      section(logger);
+   }
+}
