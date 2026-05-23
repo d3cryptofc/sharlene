f4c5065dcb27ffa8eb501fb0e951f7329731c560
Lelzin λ <d3cryptofc@gmail.com>
2026-05-23 08:08:40 -0300

    feat(io): add `walkCopy` method to process and copy jar entries
    The new `walkCopy` method allows processing JAR entries through a
    collection of `WalkCopyProcessors` and writing them to a destination JAR
    file.

--- src/main/java/com/github/d3cryptofc/sharlene/io/CharlesJarFile.java
+++ src/main/java/com/github/d3cryptofc/sharlene/io/CharlesJarFile.java
@@ -1,10 +1,18 @@
 package com.github.d3cryptofc.sharlene.io;
 
+import com.github.d3cryptofc.sharlene.Main;
 import com.github.d3cryptofc.sharlene.exception.SharleneRuntimeException;
 import com.github.d3cryptofc.sharlene.utils.JarUtils;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Enumeration;
 import java.util.Properties;
+import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
+import java.util.jar.JarOutputStream;
 import java.util.zip.ZipEntry;
 
 /** Represents a Charles JAR file and provides methods to read and patch it. */
@@ -86,5 +94,55 @@ public class CharlesJarFile implements AutoCloseable {
   public JarFile getJarFile() {
     // Return the `jarFile` field.
     return this.jarFile;
+  }
+
+  /**
+   * Walks over the entries in the JAR file and copies them to the destination JAR file using the provided processors.
+   *
+   * @param destination         The destination JAR file.
+   * @param walkCopyProcessors  The processors to use for copying each entry.
+   */
+  public void walkCopy(File destination, Collection<WalkCopyProcessor> walkCopyProcessors) {
+    // Log the start of the JAR walking copy.
+    Main.LOGGER.debug("Starting JAR walking copy.");
+
+    // Get the entries from the JAR file and iterate over them.
+    Enumeration<JarEntry> entries = this.jarFile.entries();
+    // Initialize the `entry` variable.
+    ZipEntry entry;
+    // Initialize the `content` variable to an empty byte array.
+    byte[] content = new byte[0];
+
+    // If no walk copy processors are provided, use a default one.
+    if (walkCopyProcessors.size() == 0) {
+      walkCopyProcessors = Collections.singletonList(new WalkCopyProcessor());
+    }
+
+    try (
+    // Create a `JarOutputStream` to write the output JAR file.
+    JarOutputStream jarOutputStream = new JarOutputStream(new FileOutputStream(destination))) {
+      // Iterate over the entries in the JAR file.
+      while (entries.hasMoreElements()) {
+        // Get the next entry from the JAR file.
+        entry = entries.nextElement();
+        // Log the entry being processed.
+        Main.LOGGER.debug("Received entry: {}", entry);
+
+        // Call the `accept` method of each `walkCopyProcessor` to handle the entry.
+        for (WalkCopyProcessor walkCopyProcessor : walkCopyProcessors) {
+          // Set the `content` variable to the result of the `accept` method.
+          content = walkCopyProcessor.accept(this, entry);
+        }
+        // Put the current entry into the output JAR file.
+        jarOutputStream.putNextEntry(entry);
+        // Write the content of the entry to the output JAR file.
+        jarOutputStream.write(content);
+        // Close the current entry in the output JAR file.
+        jarOutputStream.closeEntry();
+      }
+    } catch (IOException e) {
+      // Throw a `SharleneRuntimeException` if an I/O error occurs.
+      throw new SharleneRuntimeException("Failed to open JAR output stream: " + e.getMessage());
+    }
   }
 }
